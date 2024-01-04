19173badb7cdc0a47f9677c1dca6e0fd22d22e51
yiwereaminu <yiwereamin@gmail.com>
2024-01-04 15:10:12 -0800

    adds numbers infinitly

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    num_args = len(args)
+    
+    if num_args == 0:
+        print(0)
+    else:
+        result = sum(int(arg) for arg in args)
+        print(result)

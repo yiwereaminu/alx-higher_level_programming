4aa9c055871f77244e3f38cddcbd171d3496abe7
yiwereaminu <yiwereamin@gmail.com>
2024-01-09 16:15:24 -0800

    rp

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idx, element):
+def replace_in_list(my_list, idx, element):
     for i in my_list:
         if idx < 0:
             return my_list
         elif idx > len(my_list):
             return my_list
         else:
-            my_list[iidx] = element
+            my_list[idx] = element

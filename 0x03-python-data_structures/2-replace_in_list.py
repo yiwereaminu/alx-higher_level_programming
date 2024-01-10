ccebd487e20e0aadba1a6bb8610a2f37e8ce90d9
yiwereaminu <yiwereamin@gmail.com>
2024-01-09 16:11:46 -0800

    replaces elements

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@ def element_at(my_list, idx, element):
     for i in my_list:
         if idx < 0:
             return my_list
-        elif idx > len(my_lisst):
+        elif idx > len(my_list):
             return my_list
         else:
-            my_list.index(idx) = element
+            my_list[iidx] = element
